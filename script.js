aebd3c87d7e9d1fb70f2625697cb9822ae352cda
higgsboson909 <higgsbosson906@gmail.com>
2025-01-23 19:16:58 +0500

    Add all logics except changeReadStatus

---
+++ script.js
@@ -0,0 +1,38 @@
+let myLibrary = [];     // contains books array
+
+// constructor for book object
+function Book(title, author, noOfPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.noOfPages = noOfPages;
+    this.isRead = isRead;
+
+}
+
+
+function addBookToLibrary(title, author, noOfPages, isRead) {
+    const book = new Book(title, author, noOfPages, isRead);
+    myLibrary.push(book);
+    return 0;
+}
+
+function displayBook() {
+    myLibrary.forEach((myBook) => {
+        console.log(myBook);
+    });
+}
+
+function removeBook(i) {
+    myLibrary.splice(i, 1);
+}
+
+
+
+// main
+
+// add books mannualy
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false);
+addBookToLibrary('1984', 'George Orwell', 328, true);
+addBookToLibrary('Moby Dick', 'Herman Melville', 585, false);
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', 279, true);

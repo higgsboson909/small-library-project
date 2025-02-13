f0ae70ec3848450fb116bbe4bdc02a539c569699
higgsboson909 <higgsbosson906@gmail.com>
2025-02-13 11:03:56 +0500

    Display Book to Dom

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const addBookEl = document.querySelector(".add-book");
 const mainContentEl = document.querySelector(".main-content");
+const bookEl = [];
 
 let myLibrary = [];     // contains books array
-
 
 // constructor for book object
 function Book(title, author, noOfPages, isRead) {
@@ -11,6 +11,27 @@ function Book(title, author, noOfPages, isRead) {
     this.noOfPages = noOfPages;
     this.isRead = isRead;
 }
+
+Book.prototype.changeReadStatus = function (status) {
+    this.isRead = status; 
+};
+
+// main
+
+// add books mannualy
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false);
+addBookToLibrary('1984', 'George Orwell', 328, true);
+addBookToLibrary('Moby Dick', 'Herman Melville', 585, false);
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', 283, true);
+
+
+insertBooks(myLibrary);
+
+
+
+
+// methods
 
 // add books to the library
 function addBookToLibrary(title, author, noOfPages, isRead) {
@@ -29,17 +50,26 @@ function removeBook(i) {
     myLibrary.splice(i, 1);
 }
 
+// insert books to dom
+function insertBooks(bookArr) {
 
-Book.prototype.changeReadStatus = function (status) {
-    this.isRead = status; 
-};
-
-// main
-
-// add books mannualy
-addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
-addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false);
-addBookToLibrary('1984', 'George Orwell', 328, true);
-addBookToLibrary('Moby Dick', 'Herman Melville', 585, false);
-addBookToLibrary('Pride and Prejudice', 'Jane Austen', 283, true);
-
+    
+    bookArr.forEach((book) => {
+        const bookDiv = document.createElement("div");
+        const titleEl = document.createElement("div");
+        const authorEl = document.createElement("div"); 
+        const noOfPagesEl = document.createElement("div");
+        bookDiv.append(titleEl);
+        bookDiv.append(authorEl);
+        bookDiv.append(noOfPagesEl);
+        bookDiv.className = "book";
+        titleEl.className = "title";
+        authorEl.className = "author";
+        noOfPagesEl.className = "no-of-pages";
+        titleEl.innerHTML = book.title;
+        authorEl.innerHTML =  book.author;
+        noOfPagesEl.innerHTML = "p. - " + book.noOfPages;
+        bookEl.push(bookDiv);
+        mainContentEl.append(bookDiv);
+    });
+}
